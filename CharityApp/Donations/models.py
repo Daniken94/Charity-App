61eaba7c9b630e863bc8590c8a4269a2c6e68292
Kamil Klekotka <kamil.klekotka@gmail.com>
2022-02-24 12:13:22 +0100

    models Donations, Category, Institution

--- CharityApp/Donations/models.py
+++ CharityApp/Donations/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.forms import NullBooleanField
+from django.conf import settings
 
-# Create your models here.
+# czy ma byc nullboleanfield
+# czy ma być auth_user_model
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=120)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+
+
+
+class Institution(models.Model):
+    type_choices = (
+        ("fundacja", 'fundacja'),
+        ("organizacja pozarządowa", "organizacja pozarządowa"),
+        ("zbiórka lokalna", "zbiórka lokalna"),
+    )
+
+    name = models.CharField(max_length=120)
+    description = models.TextField(default="", blank=True)
+    type = models.CharField(max_length=24, choices=type_choices, default="fundacja")
+    categories = models.ManyToManyField("Category")
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+
+
+class Donations(models.Model):
+    quantity = models.IntegerField()
+    city = models.CharField(max_length=120, verbose_name="Podaj miasto.")
+    street = models.CharField(max_length=120, verbose_name="Podaj nazwę ulicy.")
+    home_number = models.IntegerField(verbose_name="Podaj numer domu/mieszkania.")
+    phone_number = models.IntegerField()
+    zip_code = models.CharField(max_length=5, default="42680", verbose_name="Podaj kod pocztowy.")
+    pick_up_date = models.DateField(auto_now_add=False, verbose_name="Paczka będzie gotowa do odbioru dnia: ")
+    pick_up_time = models.TimeField(auto_now=False, auto_now_add=False, verbose_name="Paczka będzie gotowa do odbioru o godzinie: ")
+    pick_up_comment = models.TextField(max_length=240, verbose_name="Komentarz dla kuriera")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, null=True, default=NullBooleanField) 
+    institution = models.ForeignKey(Institution, on_delete=models.PROTECT)
+    categories = models.ManyToManyField("Category")
+  
+    class Meta:
+        verbose_name_plural = "Donations"
